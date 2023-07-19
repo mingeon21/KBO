73961d67de8a219c27560ccfb633707a6a69a397
mingeon21 <michaelmgkim@gmail.com>
2023-07-19 21:47:08 +0900

    algorithm created

--- modeling/predict.py
+++ modeling/predict.py
@@ -1,7 +1,6 @@
-from pickle import NONE
 from sklearn.linear_model import LinearRegression
 import pandas as pd
-import numpy as np
+import joblib
 
 
 def stringToFloat(ip_str):
@@ -9,36 +8,39 @@ def stringToFloat(ip_str):
     words=ip_str.split(" ")
     if '/' in words[0]:
         if words[0][0] =='1':
-            ip=ip+0.33
+            ip = ip+0.33
         else:
-            ip=ip+0.66
+            ip = ip+0.66
     else:
-        ip=ip+float(words[0])
+        ip = ip + float(words[0])
 
     if len(words)>1:
         if words[1][0] =='1':
-            ip=ip+0.33
+            ip = ip + 0.33
         else:
-            ip=ip+0.66
-    return (ip)
+            ip = ip + 0.66
+    return ip
             
 
+def train(input_file_name, output_file_name):
+    df = pd.read_csv(input_file_name, sep="\t")
+    X = df['W']
+    df['IP'] = df['IP'].apply(stringToFloat)
+    y = df['IP']
+    print(X)
+    print(y)
 
-df = pd.read_csv('data/PitchingStats_2023.tsv', sep="\t")
+    line_fitter = LinearRegression()
+    line_fitter.fit(X.values.reshape(-1,1), y)
 
-print (df)
-
-X=[x for x in df["W"]]
-X=np.array(X)
-y = [[stringToFloat(y) for y in df["IP"]]]
-y=np.array(y)
-print(y)
-print(X)
+    joblib.dump(line_fitter, output_file_name) 
 
 
-line_fitter = LinearRegression()
-line_fitter.fit(X.reshape(1,-1), y)
+def main():
+    train('PitchingStats_2023.tsv', 'kbo_model.joblib')
+    model = joblib.load('kbo_model.joblib')
+    predict_val = model.predict([[5], [6]])
+    print(predict_val)
 
-print(line_fitter.predict([10]))
-print (line_fitter.coef_)
-print (line_fitter.intercept_)
+
+main()
