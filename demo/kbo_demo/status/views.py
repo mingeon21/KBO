6f31c6d84b5ce4301bc560166731fb423a163439
mingeon21 <michaelmgkim@gmail.com>
2023-08-07 23:13:55 +0900

    Example Graph with 2 Different X values

--- demo/kbo_demo/status/views.py
+++ demo/kbo_demo/status/views.py
@@ -2,10 +2,28 @@ from django.http import HttpResponse
 from django.template import loader 
 from django.shortcuts import render 
 
+def stringToFloat(ip_str):
+    ip=0
+    words=ip_str.split(" ")
+    if '/' in words[0]:
+        if words[0][0] =='1':
+            ip = ip+0.33
+        else:
+            ip = ip+0.66
+    else:
+        ip = ip + float(words[0])
+
+    if len(words)>1:
+        if words[1][0] =='1':
+            ip = ip + 0.33
+        else:
+            ip = ip + 0.66
+    return ip
+
 def loadfile(request):
     template = loader.get_template('status/table.html')
     pitcherInfo=[] 
-    f=open('status/data/PitchingStats_2023.tsv')
+    f=open('status/data/PitchingStats_2022.tsv')
     for line in f.readlines():
         words=line.split('\t')
         pitcherInfo.append(words)
@@ -15,19 +33,24 @@ def loadfile(request):
     return HttpResponse(template.render(context, request))
 
 def result(request):
-    w, era = makeData()
-    context = {'w': w, 'era': era}
+    eraData, whipData = makeData()
+    context = {'eraData': eraData, 'whipData': whipData}
     return render(request, 'result.html', context )
 
 
 def makeData():
-    era =[] 
-    w = []
-    f=open('status/data/PitchingStats_2023.tsv')
+    eraData = []
+    whipData = []
+    f=open('status/data/PitchingStats_2022.tsv')
     for line in f.readlines() [1:]:
         words=line.split('\t')
-        w.append(int (words[5]))
-        if words[3] == '-':
-            words[3] = '200'
-        era.append(float(words[3]))
-    return w, era 
+        ip= stringToFloat(words[10])
+        if words[3] == '-' or float(words[3])>50:
+            words[3] = '-5'
+        era = float(words[3])
+        whip = float(words[-1])
+        eraData.append([ip, era])
+        whipData.append([ip,whip])
+    return eraData, whipData
+
+
